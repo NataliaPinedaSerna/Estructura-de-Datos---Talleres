ae6f3998390879a0b50af8fdb4f877f74c551197
NataliaPinedaSerna <79171560+NataliaPinedaSerna@users.noreply.github.com>
2021-02-16 19:34:53 -0500

    Add files via upload

---
+++ 2 - Second Homework/JS/Images Collections.js
@@ -0,0 +1,26 @@
+let wildLifeCollection = [
+    {number=1 , id="1" , name="1" , image="IMG\WILD LIFE COLLECTION\1-Urchin.png"} ,
+    {number=2 , id="2" , name="2" , image="IMG\WILD LIFE COLLECTION\2-Lemur.png"} ,
+    {number=3 , id="3" , name="3" , image="IMG\WILD LIFE COLLECTION\3-Owl.png"} ,
+    {number=4 , id="4" , name="4" , image="IMG\WILD LIFE COLLECTION\4-Wairus.png"} ,
+    {number=5 , id="5" , name="5" , image="IMG\WILD LIFE COLLECTION\5-Camel.png"} ,
+    {number=6 , id="6" , name="6" , image="IMG\WILD LIFE COLLECTION\6-Hippopotamus.png"}
+];
+
+let familyMembersCollection = [
+    {number=1 , id="1" , name="1" , image="IMG\FAMILY MEMBERS COLLECTION\1-Girl.png"} ,
+    {number=2 , id="2" , name="2" , image="IMG\FAMILY MEMBERS COLLECTION\2-Boy.png"} ,
+    {number=3 , id="3" , name="3" , image="IMG\FAMILY MEMBERS COLLECTION\3-Mom.png"} ,
+    {number=4 , id="4" , name="4" , image="IMG\FAMILY MEMBERS COLLECTION\4-Dad.png"} ,
+    {number=5 , id="5" , name="5" , image="IMG\FAMILY MEMBERS COLLECTION\5-GrandMother.png"} ,
+    {number=6 , id="6" , name="6" , image="IMG\FAMILY MEMBERS COLLECTION\6-GranadFather.png"}
+];
+
+let numbersCollection = [
+    {number=1 , id="1" , name="1" , image="IMG\NUMBERS COLLECTION\1-One.png"} ,
+    {number=2 , id="2" , name="2" , image="IMG\NUMBERS COLLECTION\2-Two.png"} ,
+    {number=3 , id="3" , name="3" , image="IMG\NUMBERS COLLECTION\3-Three.png"} ,
+    {number=4 , id="4" , name="4" , image="IMG\NUMBERS COLLECTION\4-Four.png"} ,
+    {number=5 , id="5" , name="5" , image="IMG\NUMBERS COLLECTION\5-Five.png"} ,
+    {number=6 , id="6" , name="6" , image="IMG\NUMBERS COLLECTION\6-Six.png"}
+];
